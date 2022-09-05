9e31fb7196acbc64ca7a6673c5f5eb492eecc175
babar <bapt09.rousseau@gmail.com>
2022-09-05 23:50:54 +0200

    end and corner moves

--- vshd.py
+++ vshd.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy
 import math
 
 
@@ -15,18 +14,19 @@ def end_movement(lattice, residue):
         End residue to move.
     """
     neighbor_residue = lattice.protein.get_neighbors(residue)
-    if neighbor_residue.length != 1:
+    if len(neighbor_residue) != 1:
         raise ValueError(
             "Residue isn't an end residue as it has not exactly one neighbor")
 
-    empty_neighbors = lattice.empty_neighbors(neighbor_residue)
+    empty_neighbors = lattice.empty_neighbors(neighbor_residue[0].get_coords())
 
     # if another position is available
     if empty_neighbors:
-        random_neighbor = np.random.choice(empty_neighbors)
-        return random_neighbor
+        random_neighbor = np.random.choice(len(empty_neighbors))
+        return empty_neighbors[random_neighbor]
     else:
         return None
+
 
 def corner_movement(lattice, residue):
     """
@@ -40,15 +40,17 @@ def corner_movement(lattice, residue):
         Corner residue to move.
     """
     neighbors_residues = lattice.protein.get_neighbors(residue)
-    
+
     # corner residues have exactly two neighbors
-    if neighbors_residues.length == 2:
-        neighbors_residues_coords = tuple(res.get_coords() for res in neighbors_residues)
+    if len(neighbors_residues) == 2:
+        neighbors_residues_coords = tuple(
+            res.get_coords() for res in neighbors_residues)
 
         # check that the two neighbors form a corner
         if math.prod([abs(i - j) for i, j in zip(*neighbors_residues_coords)]):
-            corner_position = tuple(abs(i + j) - k for i, j, k in zip(*neighbors_residues_coords, residue.get_coords()))
-            
+            corner_position = tuple(abs(
+                i + j) - k for i, j, k in zip(*neighbors_residues_coords, residue.get_coords()))
+
             # if the corner position is available
             if lattice.is_empty(corner_position):
                 return corner_position
