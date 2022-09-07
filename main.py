e6d0351846a967805da66071f78302a81c9d3cbb
babar <bapt09.rousseau@gmail.com>
2022-09-07 18:05:05 +0200

    small changes

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from MCsearch import MCsearch
 
 protein = Protein("PHPPHPHPHPPHPPHPPHPHPPHPPHPHPPHP")
 lattice = Lattice(protein, "linear")
-print("Initial lattice:")
+
+print(f"Initial lattice with energy of {lattice.calculate_energy()}")
 lattice.draw_grid()
-lattice = MCsearch(1000, lattice, 200)
-print("Final lattice:")
+
+lattice = MCsearch(1000, lattice, 300)
+
+print(f"Final lattice with energy of {lattice.calculate_energy()}")
 lattice.draw_grid()
